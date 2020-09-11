d825a83b484e31587b30edef0974f48f1c35b2ab
GayatriPardhi <57560918+GayatriPardhi@users.noreply.github.com>
2020-09-11 13:03:29 +0530

    Create program3.js

---
+++ Day4 Assignment/program3/program3.js
@@ -0,0 +1,26 @@
+let array = [
+  {
+    name: "paul",
+    age: 30,
+    country: "England",
+    hobbies: ["Cooking", "drawing", "programming"],
+  },
+  {
+    name: "manica",
+    age: 28,
+    country: "America",
+    hobbies: ["cricket", "dancing", "cooking"],
+  },
+  {
+    name: "ross",
+    age: 25,
+    country: "china",
+    hobbies: ["reading", "laughing", "coding"],
+  },
+];
+
+function arrayOfObjects() {
+  console.log(array);
+}
+
+arrayOfObjects();
